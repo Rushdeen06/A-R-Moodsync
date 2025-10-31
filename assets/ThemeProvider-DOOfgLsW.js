e39b53ee44f4ef902665b78238b7fab92d28114a
Rushdeen06 <rushdeen.white@mrisoftware.com>
2025-10-31 04:57:26 +0200

    deploy: publish complete app with all features

---
+++ assets/ThemeProvider-DOOfgLsW.js
@@ -0,0 +1,1 @@
+import{r as t}from"./react-TREEgIqT.js";const o=t.createContext({theme:"light",toggleTheme:()=>{}}),s=()=>{const e=t.useContext(o);if(!e)throw new Error("useTheme must be used within a ThemeProvider");return e};export{s as u};
